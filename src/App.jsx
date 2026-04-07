aa20aaecf170bd8945a9ce29698ba4282b1698ca
anshikazerologic <anshika@zerologic.io>
2026-04-07 15:35:53 +0530

    change in css

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 const App = () => {
   return (
     <main className="container">
+
       
-     
       <section className="video-section">
-        <video autoPlay loop muted playsInline>
-          <source src="/zerologicComingsoon.mp4" type="video/mp4" />
-          Your browser does not support HTML5 video.
-        </video>
+        <div className="video-wrapper">
+          <video autoPlay loop muted playsInline>
+            <source src="/zerologicComingsoon.mp4" type="video/mp4" />
+            Your browser does not support HTML5 video.
+          </video>
+        </div>
       </section>
 
-     
+      
       <section className="text-section">
         <p className="coming-soon">coming soon</p>
       </section>
